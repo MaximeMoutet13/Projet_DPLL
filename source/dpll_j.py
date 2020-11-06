81097bb5be186f4ccb910cdfbd70465d7359f51f
maxime <maxime.moutet@etu.univ-amu.fr>
2020-11-06 17:39:07 +0100

    y

--- source/dpll_j.py
+++ source/dpll_j.py
@@ -31,7 +31,7 @@ def dpll(heuristic_choice, clauses, literals, clauses_lenght=None, literals_stat
     l = mono_choice(literals, literals_state, clauses, clauses_state, clauses_lenght)
     if isinstance(l, int):
         """Mono-litteral or monotone litteral"""
-        literals_state = update_literal_state(l, literals_state)
+        literals_state = update_literal_state(literals_state, l)
         running_literals = [l] + running_literals
         clauses_state, clauses_lenght = update_clause(clauses, clauses_state, clauses_lenght, l)
         if (state == 0 for state in clauses_state):
@@ -42,14 +42,14 @@ def dpll(heuristic_choice, clauses, literals, clauses_lenght=None, literals_stat
     else:
         """Heuristic choice of a litteral"""
         l_1 = heuristic_choice(literals, literals_state, clauses, clauses_state)
-        literals_state_1 = update_literal_state(l_1, literals_state)
+        literals_state_1 = update_literal_state(literals_state, l_1)
         running_literals_1 = [l_1] + running_literals
         clauses_state_1, clauses_lenght_1 = update_clause(clauses, clauses_state, clauses_lenght, l_1)
         if l % 2 == 0:
             l_2 = l_1 + 1
         else:
             l_2 = l_1 - 1
-        literals_state_2 = update_literal_state(l_2, literals_state)
+        literals_state_2 = update_literal_state(literals_state, l_2)
         running_literals_2 = [l_2] + running_literals
         clauses_state_2, clauses_lenght_2 = update_clause(clauses, clauses_state, clauses_lenght, l_2)
         if (state == 0 for state in clauses_state):
